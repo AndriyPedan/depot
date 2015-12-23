b7d2c9b78604666ab7297ad0515c9e2e69d7f743
AndriyPedan <andriypedan.p@gmail.com>
2015-12-23 15:23:40 +0200

    Add order fields, validations, email sender

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,6 @@
+class OrderMailer < ActionMailer::Base
+  default from: 'support@geekhub.com'
+  def new_order
+    mail(to: 'admin@geekhub.com', subject: 'New order')
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ActiveRecord::Base
   has_many :products, class_name: 'OrderProduct'
 
+  validates :email, presence: true, format: /@/
+  validates :phone, presence: true, format: { with: /\+\d{12}/, message: 'Format: +380XXXXXXXXX' }
+  validates :address, presence: true
+
   accepts_nested_attributes_for :products
 
   before_save :cal_total
